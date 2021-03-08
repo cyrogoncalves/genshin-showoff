524f8df704ec533d49378a7222966570c6219212
cyro <cyro.goncalves@globalhitss.com.br>
2021-03-07 22:44:19 -0300

    artifact service

---
+++ src/artifact.service.spec.ts
@@ -0,0 +1,62 @@
+import { ArtifactService } from './artifact.service';
+import { Artifact } from './model';
+
+const godlyArtifact: Artifact = {
+  set: 'Crimson', // Crimson Witch of Flames
+  // exp: 270475,
+  level: 20,
+  type: 'Flower',
+  rarity: 5,
+  mainStat: 'HP',
+  subStats: {
+    'CR': 15.6,
+    'CD': 23.4,
+    'ATK%': 5.8,
+    'ER': 6.5
+  }
+};
+
+
+describe('ArtifactService', () => {
+
+  it('possible Values For 1 Roll on 5*', () => {
+    const possibleSums = ArtifactService.possibleCombinations(4, 1);
+    expect(possibleSums).toEqual([[0], [1], [2], [3]]);
+  })
+
+  it('possible Values For 2 Rolls on 5*', () => {
+    const possibleSums = ArtifactService.possibleCombinations(4, 2);
+    console.log(possibleSums);
+    expect(possibleSums).toEqual([[0, 0], [0, 1], [0, 2], [0, 3], [1, 1], [1, 2], [1, 3], [2, 2], [2, 3], [3, 3]]);
+  });
+
+  it('possible Values For 3 Rolls on 5*', () => {
+    const possibleSums = ArtifactService.possibleCombinations(4, 3);
+    console.log(possibleSums);
+    expect(possibleSums).toEqual([[0, 0, 0], [0, 0, 1], [0, 0, 2], [0, 0, 3], [0, 1, 1], [0, 1, 2], [0, 1, 3], [0, 2, 2], [0, 2, 3], [0, 3, 3],
+      [1, 1, 1], [1, 1, 2], [1, 1, 3], [1, 2, 2], [1, 2, 3], [1, 3, 3],
+      [2, 2, 2], [2, 2, 3], [2, 3, 3],
+      [3, 3, 3]]);
+  });
+
+  it('estimate substat rolls for ER:6.5%', () => {
+    const estimate = ArtifactService.estimateSubstatRolls('ER', 6.5);
+    expect(estimate).toEqual([3])
+  });
+
+  it('estimate substat rolls for CD:23.4%', () => {
+    const estimate = ArtifactService.estimateSubstatRolls('CD', 23.4);
+    expect(estimate).toEqual([3, 3, 3])
+  });
+
+  it('estimate substat rolls for CR:15.6%', () => {
+    const estimate = ArtifactService.estimateSubstatRolls('CR', 15.6);
+    expect(estimate).toEqual([3, 3, 3, 3])
+  });
+
+  it("estimate rolls for artifact", () => {
+    const estimate = ArtifactService.estimateRolls(godlyArtifact);
+    expect(estimate).toEqual([[3, 3, 3, 3], [3, 3, 3], [3], [3]]);
+  });
+});
+

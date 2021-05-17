8c35025f1d9c775e3abc977d90b49b3b5bc3acc4
cyro <cyro.goncalves@globalhitss.com.br>
2021-05-17 18:00:19 -0300

    createArtifactsEmbed

--- src/characterBuild.command.ts
+++ src/characterBuild.command.ts
@@ -1,9 +1,11 @@
 import * as Discord from 'discord.js';
+import { MessageEmbed } from 'discord.js';
 import * as ShowoffEmoji from "../assets/discordEmotes.json";
 import * as WEAPONS from '../assets/weapons.json';
 import * as ARTIFACTS from "../assets/artifacts.json";
 import * as CHARACTERS from "../assets/characters.json";
 import {
+  Artifact,
   artifactTypeNames,
   CharacterBuild,
   Level,
@@ -37,11 +39,11 @@ const getAscensionEmotes = (ascension: number): string => {
   ].join("");
 }
 
-const getStatDisplay = (statType: StatType, value: number, showName = false): string => {
-  const displayValue2 = ['HP', 'DEF', 'ATK', 'EM'].includes(statType)
+const getStatDisplay = (statType: StatType, value: number): string => {
+  const displayValue = ['HP', 'DEF', 'ATK', 'EM'].includes(statType)
       ? Math.round(value) : `${Math.round(value * 10 || 0) / 10}%`;
   const emote = ShowoffEmoji[statLabelMap[statType] || statType] || statType;
-  return `${emote} ${showName ? `**${statType}:**` : ""} ${displayValue2}`;
+  return `${emote} **${statType}:** ${displayValue}`;
 }
 
 const parseBuild = (args: string[], build: CharacterBuild, mode = "Build"):
@@ -179,52 +181,52 @@ const exe = async (message: Discord.Message, args: string[]) => {
       attachment:'assets/noelle-avatar.png', // TODO
       name:'char.png'
     }]
-  });
+  }).then(() => message.channel.send(createArtifactsEmbed(build.artifacts)));
 }
 
 const createEmbed = (build: CharacterBuild): Discord.MessageEmbed => {
   const character = CharacterBuildService.getCharacter(build.characterName);
-  const embed = new Discord.MessageEmbed().setColor('#ffbf00')
-      .setTitle(`${ShowoffEmoji[character.element.toLowerCase()] || ""} ${character.name} C${build.constellation}`);
-
   const buildStats = CharacterBuildService.calculateStats(build);
   const extraStatKeys = Object.entries(buildStats).filter(([k]) => !subStatNames.includes(k));
-  embed.addField(`(level ${build.level} ${getAscensionEmotes(build.ascension)})`,
-      ["HP", "ATK", "DEF", "EM"].map(s => getStatDisplay(s, buildStats[s], true)).join("\n"), true);
-  embed.addField("\u200B", [
-    `${ShowoffEmoji.ER} **ER:** ${Math.round(buildStats["ER"] * 10 || 0) / 10}%`,
-    `${ShowoffEmoji.CR} **Crit Rate:** ${Math.round(buildStats["CR"] * 10 || 0) / 10}%`,
-    `${ShowoffEmoji.CD} **Crit DMG:** ${Math.round(buildStats["CD"] * 10 || 0) / 10}%`,
-    ...extraStatKeys.map(([k, v]) => getStatDisplay(k, v, true))
-  ].join("\n"), true)
-  embed.addField("Talents:", [
-    `Normal attack: ${build.talentLevels.normalAttack}`,
-    `Elemental skill: ${build.talentLevels.elementalSkill}`,
-    `Elemental burst: ${build.talentLevels.elementalBurst}`
-  ].join("\n"), true); // TODO crowns
-
   const weaponModel = WEAPONS.find(w => w.name === build.weapon.name) as WeaponModel;
-  const weaponTypeEmote = ShowoffEmoji[weaponModel.type.toLowerCase()];
   const weaponStats = WeaponService.calculateStats(build.weapon);
-  const ascension = getAscensionEmotes(build.weapon.ascension);
-  const weaponAtk = `${ShowoffEmoji.ATK} **ATK:** ${weaponStats["ATK"]}`;
-  const substat = Object.entries(weaponStats).filter(([k]) => k !== "ATK")
-      .map(([k, v]) => getStatDisplay(k, v, true)).join("\n");
-  embed.addField(`${weaponTypeEmote} ${weaponModel.name} R${build.weapon.refinement}`,
-      `(level ${build.weapon.level} ${ascension})\n${weaponAtk}\n${substat}`, true);
 
-  build.artifacts.forEach(art => {
-    const title = `${ShowoffEmoji[art.type]} ${art.set}`;
-    const desc = `**+${art.level} ${getStatDisplay(art.mainStat, ArtifactService.getMainStatValue(art))}**`
-    const substats = Object.entries(art.subStats).map(([k, v]) => {
-      const rolls = ArtifactService.estimateSubstatRolls(k, v, art.rarity).map(i => ShowoffEmoji[`roll${i+1}`])
-      return `${getStatDisplay(k, v)} ${rolls.reverse().join("")}`;
-    }).join('\n');
-    embed.addField(title, `${desc}\n${substats}`, true);
-  });
-
-  return embed;
+  return new Discord.MessageEmbed().setColor('#ffbf00')
+      .setTitle(`${ShowoffEmoji[character.element.toLowerCase()] || ""} ${character.name} C${build.constellation}`)
+      .addField(`(level ${build.level} ${getAscensionEmotes(build.ascension)})`, [
+        ...["HP", "ATK", "DEF", "EM"].map(s => getStatDisplay(s, buildStats[s])),
+        `${ShowoffEmoji.ER} **ER:** ${Math.round(buildStats["ER"] * 10 || 0) / 10}%`,
+        `${ShowoffEmoji.CR} **Crit Rate:** ${Math.round(buildStats["CR"] * 10 || 0) / 10}%`,
+        `${ShowoffEmoji.CD} **Crit DMG:** ${Math.round(buildStats["CD"] * 10 || 0) / 10}%`,
+        ...extraStatKeys.map(([k, v]) => getStatDisplay(k, v))
+      ].join("\n"), true)
+      .addField("Talents:", [
+        `Normal attack: ${build.talentLevels.normalAttack}`,
+        `Elemental skill: ${build.talentLevels.elementalSkill}`,
+        `Elemental burst: ${build.talentLevels.elementalBurst}`,  // TODO crowns
+        "",
+        `**${ShowoffEmoji[weaponModel.type.toLowerCase()]} ${weaponModel.name} R${build.weapon.refinement}**`,
+        `(level ${build.weapon.level} ${(getAscensionEmotes(build.weapon.ascension))})`,
+        `${ShowoffEmoji.ATK} **ATK:** ${weaponStats["ATK"]}`,
+        ...Object.entries(weaponStats).filter(([k]) => k !== "ATK")
+            .map(([k, v]) => getStatDisplay(k, v))
+      ].join("\n"), true);
 };
+
+const createArtifactsEmbed = (artifacts: Artifact[]): Discord.MessageEmbed => {
+  const embed = new MessageEmbed()
+      .setTitle("Artifacts")
+      .setColor('#ffbf00') // TODO by rarity
+  artifacts.forEach(art => {
+    embed.addField(`${ShowoffEmoji[art.type]} ${art.set} +${art.level}\n(${art.rarity} :star:) **${art.mainStat}: ${ArtifactService.getMainStatValue(art)}**`,
+          Object.entries(art.subStats).map(([statType, value]) => {
+    const rolls = ArtifactService.estimateSubstatRolls(statType, value, art.rarity);
+    const rollsStr = rolls.map(i => ShowoffEmoji[`roll${i+1}`]).reverse().join("");
+    return `${getStatDisplay(statType, value)} ${rollsStr}`;
+  }).join("\n"), true)});
+  embed.addField("\u200B", "\u200B", true) // hack!!
+  return embed;
+}
 
 export const CharacterBuildCommand = {
   createEmbed,
